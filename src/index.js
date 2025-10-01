b79dd47543f3f4ac2ea20efa2fa3d34e42a24694
dianannperla <waafirmelyza@hotmail.com>
2025-10-01 16:17:38 +0100

    feat: initial commit for src/index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Chào mừng đến với Trang Web Phụ Kiện</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
